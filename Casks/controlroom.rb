734a136d7b147b38e5ff22d7e62b271c6736b83f
Bastian Inuk Christensen <bastian@inuk.blog>
2020-02-13 20:42:23 +0100

    Update controlroom.rb

--- Casks/controlroom.rb
+++ Casks/controlroom.rb
@@ -1,6 +1,6 @@
 cask 'ControlRoom' do
   version '1.0'
-  sha256 ''
+  sha256 'ee87176ef05c5ea3a7f5848f0500b13ec914ae578225164990f0a106335c6577'
 
   url "https://github.com/Human-Entertainment/ControlRoom/releases/download/1.0/ControlRoom.app.zip"
   name 'ControlRoom'
