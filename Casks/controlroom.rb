5f074e33388c6132d16694c5a0f8593c59e4a635
Bastian Inuk Christensen <bastian@inuk.blog>
2020-02-13 20:38:36 +0100

    Create controlroom.rb

---
+++ Casks/controlroom.rb
@@ -0,0 +1,10 @@
+cask 'ControlRoom' do
+  version '1.0'
+  sha256 ''
+
+  url "https://github.com/Human-Entertainment/ControlRoom/releases/download/1.0/ControlRoom.app.zip"
+  name 'ControlRoom'
+  homepage ''
+
+  app 'ControlRoom.app'
+end
